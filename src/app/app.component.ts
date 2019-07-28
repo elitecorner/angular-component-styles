ee21afa41283b30971770c18b0755389694d1b4a
elitecorner <elite.corner2018@gmail.com>
2019-07-28 14:10:13 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styles: [`
-   h1 { 
-     color: red; 
-   }
-   h2 { 
-     color:green;
-   }
-`]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
  
