e1dc707dbe597fcd69a7bd5c7e65a0d9509ea32d
elitecorner <elite.corner2018@gmail.com>
2019-07-28 14:06:38 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styles: [`
+   h1 { 
+     color: red; 
+   }
+   h2 { 
+     color:green;
+   }
+`]
+})
+export class AppComponent  {
+ 
+}
